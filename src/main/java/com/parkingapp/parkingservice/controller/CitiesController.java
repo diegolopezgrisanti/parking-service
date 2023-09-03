ae3e883ceed9425f262cf423881cceb8cc51124f
dlopezgrisan <diego.lopezgrisanti@mercadolibre.com>
2023-09-03 13:58:52 -0300

    add swagger documentacion in cities controller

--- src/main/java/com/parkingapp/parkingservice/controller/CitiesController.java
+++ src/main/java/com/parkingapp/parkingservice/controller/CitiesController.java
@@ -2,8 +2,15 @@ package com.parkingapp.parkingservice.controller;
 
 import com.parkingapp.parkingservice.dto.CitiesResponse;
 import com.parkingapp.parkingservice.dto.CityDTO;
+import com.parkingapp.parkingservice.dto.ErrorResponse;
 import com.parkingapp.parkingservice.model.City;
 import com.parkingapp.parkingservice.service.CitiesService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +21,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/cities")
+@Tag(name = "Cities", description = "All about cities")
 public class CitiesController {
     private final CitiesService citiesService;
 
@@ -21,6 +29,38 @@ public class CitiesController {
         this.citiesService = citiesService;
     }
 
+    @Operation(summary = "List cities")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Successful response",
+                    content = {
+                            @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation =CitiesResponse.class)
+                            )
+                    }),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Bad request",
+                    content = {
+                            @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ErrorResponse.class)
+                            )
+                    }
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "Internal server error",
+                    content = {
+                            @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ErrorResponse.class)
+                            )
+                    }
+            )
+    })
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public CitiesResponse getCities() {
--- src/main/java/com/parkingapp/parkingservice/dto/CitiesResponse.java
+++ src/main/java/com/parkingapp/parkingservice/dto/CitiesResponse.java
@@ -1,9 +1,14 @@
 package com.parkingapp.parkingservice.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.util.List;
 
 public class CitiesResponse {
 
+    @Schema(
+            description = "List of cities"
+    )
     private List<CityDTO> cities;
 
     public CitiesResponse(List<CityDTO> cities) {
