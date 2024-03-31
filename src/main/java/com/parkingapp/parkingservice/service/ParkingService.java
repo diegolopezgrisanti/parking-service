661ede4a9fbec39ed6d8aad2804d862f50b25df8
fabriciodigennaro <slither33f@gmail.com>
2024-03-31 15:39:58 +0200

    implement parking Status check

--- src/main/java/com/parkingapp/parkingservice/service/ParkingService.java
+++ src/main/java/com/parkingapp/parkingservice/service/ParkingService.java
@@ -1,7 +1,12 @@
 package com.parkingapp.parkingservice.service;
 
 import com.parkingapp.parkingservice.model.Parking;
+import com.parkingapp.parkingservice.model.ParkingStatusCheck;
+
+import java.util.UUID;
 
 public interface ParkingService {
     Parking createParking(Parking parking);
+
+    ParkingStatusCheck getParkingStatusCheck(String plate, UUID parkingZoneId);
 }
--- src/main/java/com/parkingapp/parkingservice/service/ParkingServiceImpl.java
+++ src/main/java/com/parkingapp/parkingservice/service/ParkingServiceImpl.java
@@ -2,11 +2,17 @@ package com.parkingapp.parkingservice.service;
 
 import com.parkingapp.parkingservice.exceptions.ParkingZoneNotFoundException;
 import com.parkingapp.parkingservice.model.Parking;
+import com.parkingapp.parkingservice.model.ParkingStatus;
+import com.parkingapp.parkingservice.model.ParkingStatusCheck;
 import com.parkingapp.parkingservice.repository.ParkingRepository;
 import com.parkingapp.parkingservice.repository.ParkingZonesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -32,6 +38,24 @@ public class ParkingServiceImpl implements ParkingService {
 
         parkingRepository.saveParking(parking);
         return parking;
+    }
+
+    @Override
+    public ParkingStatusCheck getParkingStatusCheck(String plate, UUID parkingZoneId) {
+        List<Parking> parkings = this.parkingRepository.getTodayParkingsByPlateAndZone(plate, parkingZoneId);
+        Optional<Parking> lastParking = parkings.stream().max(Comparator.comparing(Parking::getExpiration));
+        ParkingStatus status = this.resolveParkingStatus(lastParking);
+        return new ParkingStatusCheck(
+                status,
+                lastParking.orElse(null)
+        );
+    }
+
+    private ParkingStatus resolveParkingStatus(Optional<Parking> lastParking) {
+        if (lastParking.isEmpty()) {
+            return ParkingStatus.NOT_FOUND;
+        }
+        return lastParking.get().getExpiration().isAfter(Instant.now()) ? ParkingStatus.ACTIVE : ParkingStatus.EXPIRED;
     }
 
     private boolean isParkingZoneNonExistent(UUID parkingZoneId) {
