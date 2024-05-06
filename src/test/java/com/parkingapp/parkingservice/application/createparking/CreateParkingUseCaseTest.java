2b3337b10145c0fbec9f5b7abe782ade8ab4b185
dlopezgrisan <diego.lopezgrisanti@mercadolibre.com>
2024-05-06 12:45:12 -0300

    [add] createParking test

---
+++ src/test/java/com/parkingapp/parkingservice/application/createparking/CreateParkingUseCaseTest.java
@@ -0,0 +1,43 @@
+package com.parkingapp.parkingservice.application.createparking;
+
+import com.parkingapp.parkingservice.application.getparkingbyid.GetParkingByIdUseCase;
+import com.parkingapp.parkingservice.domain.parking.Parking;
+import com.parkingapp.parkingservice.domain.parking.ParkingRepository;
+import org.junit.jupiter.api.Test;
+
+import java.time.Instant;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.mockito.Mockito.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CreateParkingUseCaseTest {
+
+    private ParkingRepository parkingRepository = mock(ParkingRepository.class);
+    private GetParkingByIdUseCase useCase = new GetParkingByIdUseCase(parkingRepository);
+
+    @Test
+    void shouldReturnCreateParking() {
+        // GIVEN
+        UUID id = UUID.randomUUID();
+        Instant mock_instant = Instant.parse("1970-01-01T00:00:00Z");
+        Parking parking = new Parking(
+                id,
+                UUID.randomUUID(),
+                "4316KNN",
+                "mock@test.com",
+                mock_instant
+
+        );
+        when(parkingRepository.getParkingById(id)).thenReturn(Optional.of(parking));
+
+        // WHEN
+        Optional<Parking> result = useCase.execute(id);
+
+        // THEN
+        assertThat(result).isPresent().contains(parking);
+
+        verify(parkingRepository, times(1)).getParkingById(id);
+    }
+}
