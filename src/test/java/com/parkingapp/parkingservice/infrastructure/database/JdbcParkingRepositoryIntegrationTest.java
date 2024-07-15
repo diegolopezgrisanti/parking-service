01c5b65f7319657af3c4e615f2e141aee5511e7f
Sol Latorre <solbeor@gmail.com>
2024-07-15 19:14:06 +0200

    add tests for getTodayParkingsByPlateAndZone method (#24)

--- src/test/java/com/parkingapp/parkingservice/infrastructure/database/JdbcParkingRepositoryIntegrationTest.java
+++ src/test/java/com/parkingapp/parkingservice/infrastructure/database/JdbcParkingRepositoryIntegrationTest.java
@@ -4,13 +4,16 @@ import com.parkingapp.parkingservice.domain.parking.Parking;
 import com.parkingapp.parkingservice.domain.parking.ParkingRepository;
 import com.parkingapp.parkingservice.infrastructure.fixtures.initializers.testannotation.IntegrationTest;
 import com.parkingapp.parkingservice.infrastructure.fixtures.initializers.testannotation.WithPostgreSql;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.test.jdbc.JdbcTestUtils;
 
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -26,17 +29,28 @@ public class JdbcParkingRepositoryIntegrationTest {
     @Autowired
     private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
+    private String plate = "DUMMY";
+    private UUID parkingZoneId = UUID.randomUUID();
+    private Instant now = Instant.now().truncatedTo(ChronoUnit.MILLIS);
+
+    @BeforeEach
+    void setUp() {
+        JdbcTestUtils.deleteFromTables(
+                namedParameterJdbcTemplate.getJdbcTemplate(),
+                "parking"
+        );
+    }
+
     @Test
     public void shouldSaveParking() {
         // GIVEN
         UUID parkingId = UUID.randomUUID();
-        UUID parkingZoneId = UUID.randomUUID();
         Parking expectedParking = new Parking(
                 parkingId,
                 parkingZoneId,
                 "ABC123",
                 "dummy@email.com",
-                Instant.now().truncatedTo(ChronoUnit.MILLIS)
+                now
         );
 
         givenExitingParkingZone(parkingZoneId);
@@ -47,6 +61,115 @@ public class JdbcParkingRepositoryIntegrationTest {
         // THEN
         Optional<Parking> result = parkingRepository.getParkingById(parkingId);
         assertThat(result).isEqualTo(Optional.of(expectedParking));
+    }
+
+    @Test
+    void shouldNotFindTodayParkingsWhenThereIsNoMatchForParkingZone() {
+        // GIVEN
+        UUID notMatchingParkingZoneId = UUID.randomUUID();
+        givenExitingParkingZone(notMatchingParkingZoneId);
+        Parking parking = new Parking(
+                UUID.randomUUID(),
+                notMatchingParkingZoneId,
+                plate,
+                "dummy@email.com",
+                now
+        );
+        parkingRepository.saveParking(parking);
+
+        // WHEN
+        List<Parking> result = parkingRepository.getTodayParkingsByPlateAndZone(plate, parkingZoneId);
+
+        // THEN
+        assertThat(result).isEmpty();
+    }
+
+    @Test
+    void shouldNotFindTodayParkingsWhenThereIsNoMatchForPlate() {
+        // GIVEN
+        givenExitingParkingZone(parkingZoneId);
+        Parking parking = new Parking(
+                UUID.randomUUID(),
+                parkingZoneId,
+                "NOT_MATCH",
+                "dummy@email.com",
+                now
+        );
+        parkingRepository.saveParking(parking);
+
+        // WHEN
+        List<Parking> result = parkingRepository.getTodayParkingsByPlateAndZone(plate, parkingZoneId);
+
+        // THEN
+        assertThat(result).isEmpty();
+    }
+
+    @Test
+    void shouldNotFindParkingsThatExpiredBeforeToday() {
+        // GIVEN
+        givenExitingParkingZone(parkingZoneId);
+        Parking parking = new Parking(
+                UUID.randomUUID(),
+                parkingZoneId,
+                plate,
+                "dummy@email.com",
+                now.minus(1, ChronoUnit.DAYS)
+        );
+        parkingRepository.saveParking(parking);
+
+        // WHEN
+        List<Parking> result = parkingRepository.getTodayParkingsByPlateAndZone(plate, parkingZoneId);
+
+        // THEN
+        assertThat(result).isEmpty();
+    }
+
+    @Test
+    void shouldFindTodayParkings() {
+        // GIVEN
+        givenExitingParkingZone(parkingZoneId);
+        Parking parking = new Parking(
+                UUID.randomUUID(),
+                parkingZoneId,
+                plate,
+                "dummy@email.com",
+                now
+        );
+        parkingRepository.saveParking(parking);
+
+        // WHEN
+        List<Parking> result = parkingRepository.getTodayParkingsByPlateAndZone(plate, parkingZoneId);
+
+        // THEN
+        assertThat(result).contains(parking);
+    }
+
+    @Test
+    void shouldFindTodayParkingsWhenThereAreMultiple() {
+        // GIVEN
+        givenExitingParkingZone(parkingZoneId);
+        Parking parking = new Parking(
+                UUID.randomUUID(),
+                parkingZoneId,
+                plate,
+                "dummy@email.com",
+                now
+        );
+        Parking parking2 = new Parking(
+                UUID.randomUUID(),
+                parkingZoneId,
+                plate,
+                "dummy@email.com",
+                now.plus(3, ChronoUnit.HOURS)
+        );
+        parkingRepository.saveParking(parking);
+        parkingRepository.saveParking(parking2);
+
+        // WHEN
+        List<Parking> result = parkingRepository.getTodayParkingsByPlateAndZone(plate, parkingZoneId);
+
+        // THEN
+        assertThat(result).contains(parking, parking2);
     }
 
     private void givenExitingParkingZone(UUID parkingZoneId) {
