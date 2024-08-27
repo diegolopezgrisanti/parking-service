cb412e7931101464b7daceb86ed0b9d8ccc22c9e
Fabricio Adrian Di gennaro <58400837+fabriciodigennaro@users.noreply.github.com>
2024-08-27 10:22:25 +0200

    Implements retry strategy when call to payment-service api (#56)
    * Implements retry strategy when call to payment-service api
    
    * Add resetAll() beforeEach test to avoid attempt verify error

--- src/main/java/com/parkingapp/parkingservice/infrastructure/client/payment/PaymentService.java
+++ src/main/java/com/parkingapp/parkingservice/infrastructure/client/payment/PaymentService.java
@@ -5,15 +5,19 @@ import com.parkingapp.parkingservice.domain.payment.Failure;
 import com.parkingapp.parkingservice.domain.payment.ParkingPaymentResponse;
 import com.parkingapp.parkingservice.domain.payment.ParkingPaymentService;
 import com.parkingapp.parkingservice.domain.payment.Successful;
+import io.github.resilience4j.retry.Retry;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import retrofit2.Response;
-
-import java.io.IOException;
 
 public class PaymentService implements ParkingPaymentService {
     private final PaymentApi paymentApi;
+    private final Retry retry;
+    private final Logger log = LogManager.getLogger(getClass());
 
-    public PaymentService(PaymentApi paymentApi) {
+    public PaymentService(PaymentApi paymentApi, Retry retry) {
         this.paymentApi = paymentApi;
+        this.retry = retry;
     }
 
     @Override
@@ -28,14 +32,22 @@ public class PaymentService implements ParkingPaymentService {
         );
 
         try {
-            Response<Void> response = paymentApi.chargeFee(paymentRequest).execute();
+            Response<Void> response = Retry.decorateCheckedSupplier(
+                    retry,
+                    () -> paymentApi.chargeFee(paymentRequest).execute()
+            ).get();
 
             if (response.isSuccessful()) {
                 return new Successful();
             } else {
                 return new Failure();
             }
-        } catch (IOException e) {
+        } catch (Throwable e) {
+            log.error(
+                    "Error while processing payment for parking with ID: {} - detail: {}",
+                    parking.getParkingId(),
+                    e
+            );
             return new Failure();
         }
     }
